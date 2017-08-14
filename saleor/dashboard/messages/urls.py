419a7d9b8bdcd7d24b23a608ebbb628914ffe8d8
p8ul <pkinuthia10@gmail.com>
2017-08-14 18:25:55 +0300

    stock reoder action button functionalities

--- saleor/dashboard/messages/urls.py
+++ saleor/dashboard/messages/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
      url(r'^$', views.list_messages, name='message_list'),
      url(r'^list/(?P<status>[\w\-]+)$', views.list_messages, name='message_list_slug'),
      url(r'^compose/$', views.write, name='compose_message'),
+     url(r'^compose/single$', views.write_single, name='compose_single_message'),
      url(r'^contacts/ajax/$', views.contacts, name='message-contacts'),
      url(r'^unread/count/$', views.unread_count, name='unread_count'),
      url(r'^add/template/$', views.add_template, name='add-sms-template'),
@@ -18,7 +19,7 @@ urlpatterns = [
      url(r'^template/delete/(?P<pk>[0-9]+)$', views.delete_template, name='delete-sms-template'),
      url(r'^read/message/(?P<pk>[0-9]+)$', views.read, name='read-message'),
      url(r'^trash/message/(?P<pk>[0-9]+)$', views.delete, name='delete-message'),
-     url(r'^delete/permanently/(?P<pk>[0-9]+)$', views.delete_permanently, name='delete-permanently'),
+     url(r'^delete/permanently/(?P<pk>[0-9]+)$', views.delete_permanently, name='delete-sms-permanently'),
 ]
 
 if settings.DEBUG:
