c484916d259183542408b56f0916fb07d89663e9
alexkiburu <alex.kiburu@glosoftgroup.com>
2017-09-08 17:06:07 +0300

    name and logo upload display in sites

--- saleor/site/models.py
+++ saleor/site/models.py
@@ -11,33 +11,34 @@ import datetime as t
 
 @python_2_unicode_compatible
 class SiteSettings(models.Model):
-    domain = models.CharField(
-        pgettext_lazy('Site field', 'domain'), max_length=100,
-        validators=[_simple_domain_name_validator],blank=True, null=True,default='')
-    name = models.CharField(pgettext_lazy('Site field', 'name'), 
-        max_length=50,blank=True, null=True)
-    header_text = models.CharField(
-        pgettext_lazy('Site field', 'header text'), max_length=200, blank=True)
-    description = models.CharField(
-        pgettext_lazy('Site field', 'site description'), max_length=500,
-        blank=True)
-    loyalty_point_equiv = models.IntegerField( pgettext_lazy('Site field', 'loyalty points equivalency'),
-        validators=[MinValueValidator(0)], default=Decimal(0)) 
-    opening_time = models.TimeField(pgettext_lazy('Site field', 'opening time'),
-        default=t.time(6, 00))
-    closing_time = models.TimeField(pgettext_lazy('Site field', 'closing time'),
-        default=t.time(21, 00))
-    sms_gateway_username = models.CharField(
-        pgettext_lazy('Site field', 'sms gateway username'), max_length=500,
-        blank=True)
-    sms_gateway_apikey = models.CharField(
-        pgettext_lazy('Site field', 'sms gateway api key'), max_length=500,
-        blank=True)
-    def __str__(self):
-        return self.name
+	domain = models.CharField(
+		pgettext_lazy('Site field', 'domain'), max_length=100,
+		validators=[_simple_domain_name_validator],blank=True, null=True,default='')
+	name = models.CharField(pgettext_lazy('Site field', 'name'),
+		max_length=50,blank=True, null=True)
+	header_text = models.CharField(
+		pgettext_lazy('Site field', 'header text'), max_length=200, blank=True)
+	description = models.CharField(
+		pgettext_lazy('Site field', 'site description'), max_length=500,
+		blank=True)
+	loyalty_point_equiv = models.IntegerField( pgettext_lazy('Site field', 'loyalty points equivalency'),
+		validators=[MinValueValidator(0)], default=Decimal(0))
+	opening_time = models.TimeField(pgettext_lazy('Site field', 'opening time'),
+		default=t.time(6, 00))
+	closing_time = models.TimeField(pgettext_lazy('Site field', 'closing time'),
+		default=t.time(21, 00))
+	sms_gateway_username = models.CharField(
+		pgettext_lazy('Site field', 'sms gateway username'), max_length=500,
+		blank=True)
+	sms_gateway_apikey = models.CharField(
+		pgettext_lazy('Site field', 'sms gateway api key'), max_length=500,
+		blank=True)
+	image = models.ImageField(upload_to='employee', null=True, blank=True)
+	def __str__(self):
+		return self.name
 
-    def available_backends(self):
-        return self.authorizationkey_set.values_list('name', flat=True)
+	def available_backends(self):
+		return self.authorizationkey_set.values_list('name', flat=True)
 
 
 @python_2_unicode_compatible
