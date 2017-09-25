048e0642ff83320382ab24ab19979b8e8f1e8754
alexK <alexkiburu@gmail.com>
2017-09-25 11:48:39 +0300

    removed unnecessary code

--- saleor/core/readmac.py
+++ saleor/core/readmac.py
@@ -10,31 +10,6 @@ class FetchMac():
     def __init__(self):
         pass
 
-    def getnumber_mac(self):
-        ''' get mac as int form then converts back to hex form'''
-
-        mac = "00:00:00:00:00:00"
-        if sys.platform == 'win32':
-            for line in os.popen("ipconfig /all"):
-                if line.lstrip().startswith('Physical Address'):
-                    mac = line.split(':')[1].strip().replace('-',':')
-                    break
-        if sys.platform == 'darwin':
-            for line in os.popen("/sbin/ifconfig"):
-                if line.find('ether') > -1:
-                    mac = line.split()[-1]
-                    break
-        else:
-            inf = netifaces.interfaces()
-            for line in os.popen("/sbin/ifconfig"):
-                for i in inf:
-                    if i.startswith('en'):
-                        if line.find(i) > -1:
-                            mac = line.split()[4]
-                            break
-
-        return mac
-
     def format_machine(self, new_id):
         formatted = str(new_id)
         bb = base64.b64encode(bytes(formatted))
@@ -42,10 +17,6 @@ class FetchMac():
         return bkey322
 
     def getnumber(self):
-        mac = get_mac()
-        h = iter(hex(mac)[2:].zfill(12))
-        mac_addr = ":".join(i + next(h) for i in h)
-        print("macadd : "+ mac_addr)
         if sys.platform == 'win32':
             mac_addr = self.windows()
             return mac_addr
