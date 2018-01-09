ddbb9f2344921873ee9ed746cbb0a7d957544bd2
sky <kampozonekenya@gmail.com>
2018-01-09 12:19:57 +0300

    reflect purchase record when editing stock

--- saleor/api/product/views.py
+++ saleor/api/product/views.py
@@ -4,16 +4,17 @@ from rest_framework.generics import (ListAPIView,
                                      CreateAPIView,
                                      RetrieveAPIView,
                                      DestroyAPIView,
-                                    )
+                                     )
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 from ...product.models import (
     Product,
     ProductVariant,
     Stock,
-    )
-from ...sale.models import (Sales, 
-                            Terminal, 
+)
+from ...sale.models import (Sales,
+                            Terminal,
                             TerminalHistoryEntry,
                             DrawerCash)
 from ...customer.models import Customer
@@ -21,17 +22,17 @@ from .serializers import (
     CreateStockSerializer,
     ProductStockListSerializer,
     ProductListSerializer,
-    SalesSerializer, 
+    SalesSerializer,
     SalesListSerializer,
-     )
+)
 from rest_framework import generics
 
 from ...decorators import user_trail
 import logging
+
 debug_logger = logging.getLogger('debug_logger')
 info_logger = logging.getLogger('info_logger')
 error_logger = logging.getLogger('error_logger')
-
 
 
 class CreateStockAPIView(CreateAPIView):
@@ -53,77 +54,82 @@ class SalesCreateAPIView(generics.CreateAPIView):
     queryset = Sales.objects.all()
     serializer_class = SalesSerializer
 
-    def perform_create(self, serializer):              
+    def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-        user_trail(self.request.user.name,'made a sale:#'+str(serializer.data['invoice_number'])+' sale worth: '+str(serializer.data['total_net']),'add')
-        info_logger.info('User: '+str(self.request.user)+' made a sale:'+str(serializer.data['invoice_number']))
+        user_trail(self.request.user.name,
+                   'made a sale:#' + str(serializer.data['invoice_number']) + ' sale worth: ' + str(
+                       serializer.data['total_net']), 'add')
+        info_logger.info('User: ' + str(self.request.user) + ' made a sale:' + str(serializer.data['invoice_number']))
         terminal = Terminal.objects.get(pk=int(serializer.data['terminal']))
-        trail = 'User: '+str(self.request.user)+\
-                ' made a sale:'+str(serializer.data['invoice_number'])+\
-                ' Net#: '+str(serializer.data['total_net'])
+        trail = 'User: ' + str(self.request.user) + \
+                ' made a sale:' + str(serializer.data['invoice_number']) + \
+                ' Net#: ' + str(serializer.data['total_net'])
         TerminalHistoryEntry.objects.create(
-                            terminal=terminal,
-                            comment=trail,
-                            crud='deposit',
-                            user=self.request.user
-                        )
-        drawer = DrawerCash.objects.create(manager=self.request.user,                                        
-                                           user = self.request.user,
+            terminal=terminal,
+            comment=trail,
+            crud='deposit',
+            user=self.request.user
+        )
+        drawer = DrawerCash.objects.create(manager=self.request.user,
+                                           user=self.request.user,
                                            terminal=terminal,
                                            amount=serializer.data['total_net'],
                                            trans_type='sale')
-        user_trail(self.request.user.name,'made a sale:#'+str(serializer.data['invoice_number'])+' sale worth: '+str(serializer.data['total_net']),'add')
-        info_logger.info('User: '+str(self.request.user)+' made a sale:'+str(serializer.data['invoice_number']))
-        
-        
+        user_trail(self.request.user.name,
+                   'made a sale:#' + str(serializer.data['invoice_number']) + ' sale worth: ' + str(
+                       serializer.data['total_net']), 'add')
+        info_logger.info('User: ' + str(self.request.user) + ' made a sale:' + str(serializer.data['invoice_number']))
+
+
 class SalesListAPIView(generics.ListAPIView):
     serializer_class = SalesListSerializer
 
-    def get_queryset(self, *args, **kwargs):        
+    def get_queryset(self, *args, **kwargs):
         queryset_list = Sales.objects.all()
         query = self.request.GET.get('q')
         try:
             if query:
                 queryset_list = queryset_list.filter(
-                    Q(invoice_number__icontains=query)|
-                    Q(customer__name__icontains=query)|
+                    Q(invoice_number__icontains=query) |
+                    Q(customer__name__icontains=query) |
                     Q(customer__mobile__icontains=query)
-                    ).distinct()
+                ).distinct()
         except:
             print('nothing found')
         return queryset_list
 
-class CreditorsListAPIView(generics.ListAPIView):    
+
+class CreditorsListAPIView(generics.ListAPIView):
     serializer_class = SalesListSerializer
-    def get_queryset(self, *args, **kwargs):        
+
+    def get_queryset(self, *args, **kwargs):
         queryset_list = Sales.objects.filter(status='payment-pending')
         query = self.request.GET.get('q')
         try:
             if query:
                 queryset_list = queryset_list.filter(status='payment-pending').filter(
-                    Q(invoice_number__icontains=query)|
-                    Q(customer__name__icontains=query)|
-                    Q(customer__name__icontains=query)|
+                    Q(invoice_number__icontains=query) |
+                    Q(customer__name__icontains=query) |
+                    Q(customer__name__icontains=query) |
                     Q(customer__mobile__icontains=query)).distinct()
         except:
             print('nothing found')
         return queryset_list
 
 
-
 class ProductListAPIView(generics.ListAPIView):
     pagination_class = PostLimitOffsetPagination
     serializer_class = ProductListSerializer
 
-    def get_queryset(self, *args, **kwargs):        
+    def get_queryset(self, *args, **kwargs):
         queryset_list = Product.objects.all().select_related()
         query = self.request.GET.get('q')
         if query:
             queryset_list = queryset_list.filter(
-                Q(name__icontains=query)|
-                Q(variants__sku__icontains=query)|
+                Q(name__icontains=query) |
+                Q(variants__sku__icontains=query) |
                 Q(categories__name__icontains=query)
-                ).distinct()
+            ).distinct()
         return queryset_list
 
 
@@ -131,13 +137,13 @@ class SearchSkuListAPIView(generics.ListAPIView):
     pagination_class = PostLimitOffsetPagination
     serializer_class = ProductStockListSerializer
 
-    def get_queryset(self, *args, **kwargs):        
+    def get_queryset(self, *args, **kwargs):
         queryset_list = ProductVariant.objects.get_in_stock().select_related()
         query = self.request.GET.get('q')
         if query:
             queryset_list = queryset_list.filter(
-                sku__startswith=query               
-                ).distinct()
+                sku__startswith=query
+            ).distinct()
         return queryset_list
 
 
@@ -145,14 +151,14 @@ class ProductStockListAPIView(generics.ListAPIView):
     pagination_class = PostLimitOffsetPagination
     serializer_class = ProductStockListSerializer
 
-    def get_queryset(self, *args, **kwargs):        
+    def get_queryset(self, *args, **kwargs):
         queryset_list = ProductVariant.objects.get_in_stock().select_related()
         query = self.request.GET.get('q')
         if query:
             queryset_list = queryset_list.filter(
                 Q(sku__icontains=query) |
-                Q(product__name__icontains=query)|
+                Q(product__name__icontains=query) |
                 Q(product__description__icontains=query)
-                ).distinct()
+            ).distinct()
         return queryset_list
 
